546273b5e8cf8e8e3e8800df856d5ac770eab9cd
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-03 06:11:04 +0530

    Create select_data.sql

---
+++ Basics/select_data.sql
@@ -0,0 +1,8 @@
+-- Select all employees
+SELECT * FROM Employees;
+
+-- Select specific columns
+SELECT name, salary FROM Employees;
+
+-- Filter with WHERE
+SELECT * FROM Employees WHERE salary > 50000;
