233d7149eccf189a24aefd2efaf3ed3f8faed5f7
Bhagya-SA <bhagyaanjaneya@gmail.com>
2025-09-03 06:10:38 +0530

    Create insert_data.sql

---
+++ Basics/insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Employees (name, department, salary, hire_date)
+VALUES 
+('Alice', 'HR', 45000.00, '2021-01-15'),
+('Bob', 'IT', 60000.00, '2020-09-12'),
+('Charlie', 'Finance', 70000.00, '2019-05-23');
